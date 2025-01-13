faa7f116d6032cea7c2f84695cdc341e305f2c55
Sebastian-Boettcher <84092041+Sebastian-Boettcher@users.noreply.github.com>
2025-01-13 18:39:08 +0100

    Added menubar

--- localbite_client/src/App.vue
+++ localbite_client/src/App.vue
@@ -1,24 +1,69 @@
 <script setup lang="ts">
+import { ref } from "vue"
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Button from "primevue/button"
-import { Form } from '@primevue/forms';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+import Menubar from 'primevue/menubar';
+
+const items = ref([
+  {
+    label: 'Router',
+    icon: 'pi pi-palette',
+    items: [
+      {
+        label: 'Styled',
+        route: '/theming/styled'
+      },
+      {
+        label: 'Unstyled',
+        route: '/theming/unstyled'
+      }
+    ]
+  },
+  {
+    label: 'Programmatic',
+    icon: 'pi pi-link',
+    command: () => {
+      router.push('/introduction');
+    }
+  },
+  {
+    label: 'External',
+    icon: 'pi pi-home',
+    items: [
+      {
+        label: 'Vue.js',
+        url: 'https://vuejs.org/'
+      },
+      {
+        label: 'Vite.js',
+        url: 'https://vitejs.dev/'
+      }
+    ]
+  }
+]);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <Button label="Verify" />
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div class="card">
+    <Menubar :model="items">
+      <template #item="{ item, props, hasSubmenu }">
+        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon" />
+            <span>{{ item.label }}</span>
+          </a>
+        </router-link>
+        <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+          <span :class="item.icon" />
+          <span>{{ item.label }}</span>
+          <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+        </a>
+      </template>
+    </Menubar>
+  </div>
 
   <RouterView />
 </template>
