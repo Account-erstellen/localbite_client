ca9b7da855d27282d634b01daa2141ee757dce92
Sebastian-Boettcher <84092041+Account-erstellen@users.noreply.github.com>
2025-02-02 13:47:49 +0100

    Updated Project and Added Primevue with themes

--- lb_client/src/app/app.component.spec.ts
+++ lb_client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'localbite_client' title`, () => {
+  it(`should have the 'lb_client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('localbite_client');
+    expect(app.title).toEqual('lb_client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, localbite_client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lb_client');
   });
 });
